fd9772ef257d60004a0519aaf81f47e2789c39cf
Mayank <mayankmahajan936@gmail.com>
2023-09-08 17:21:52 +0530

    Go_food

---
+++ mernapp/src/Screens/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+function Home() {
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <div>Body</div>
+      <div>
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default Home;
